0baa442838ebbb68333a94cf5cc71e9fe0c6b63d
Yunus Mutlu <mutluyunus708@gmail.com>
2025-09-13 16:55:44 +0300

    Create base project structure for shopping list app

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemSatet();
+}
+
+class _NewItemSatet extends State<NewItem> {
+  final _formkey = GlobalKey<FormState>();
+  var _enteredName = "";
+
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+  void _saveItem() {
+    if (_formkey.currentState!.validate()) {
+      _formkey.currentState!.save();
+      Navigator.of(context).pop(GroceryItem(id: DateTime.now().toString(), name: _enteredName , quantity: _enteredQuantity, category: _selectedCategory));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Add a new item")),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(12),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Name"),
+                ),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length == 1 ||
+                      value.trim().length > 50) {
+                    return "Must be between 1 and 50 characters";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Quantity"),
+                      ),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return "Must be valid positive num";
+                        }
+                        return null;
+                      },
+                      initialValue: _enteredQuantity.toString(),
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      initialValue: _selectedCategory,
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                      items: categories.values
+                          .map(
+                            (category) => DropdownMenuItem(
+                              value: category,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.color,
+                                  ),
+                                  const SizedBox(width: 6),
+                                  Text(category.title),
+                                ],
+                              ),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formkey.currentState!.reset();
+                    },
+                    child: Text("Reset"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _saveItem();
+                    },
+                    child: Text("Add item"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
